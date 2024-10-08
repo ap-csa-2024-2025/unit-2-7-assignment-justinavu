080d60d1124c7cefd8f4c3d4a61f78aed99bfb64
justinavu <justinvu2008@gmail.com>
2024-10-08 10:17:11 -0700

    skibidi

--- Problem2.java
+++ Problem2.java
@@ -8,9 +8,8 @@ public class Problem2
     Integer x;
     Integer y;
 
-    System.out.println(x + y);
     System.out.println("Enter values:");
-    x = scan.nextLong();
+    x = scan.nextInt();
     y = scan.nextInt();
 
     Double avg = (double)(x + y / 2);
